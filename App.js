12e66e70928006426f107d9b961d059cc15b408b
karlonautti <karri.salonen@myy.haaga-helia.fi>
2024-08-31 22:42:08 +0300

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handlePress = () => {
+    setTodos([...todos, { key: todo }]);
+    setTodo('');
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Enter a new task...'
+        onChangeText={text => setTodo(text)}
+        value={todo}
+        >
+      </TextInput>
+      <Button 
+        color='green' 
+        title='Add' 
+        onPress={handlePress}>Add task</Button>
+      <FlatList
+      data={todos}
+      renderItem={({item}) => <Text>{item.key}</Text>}
+      ListEmptyComponent={<Text>No Data</Text>}
+      ItemSeparatorComponent={
+        <View style={styles.itemSeparator}></View>}
+      >
+      </FlatList>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    height: 40,
+    width: 200,
+    textAlign: 'center',
+    margin: 5,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  itemSeparator: {
+    height: 1,
+    backgroundColor: 'black',
+    margin: 5,
   },
 });
